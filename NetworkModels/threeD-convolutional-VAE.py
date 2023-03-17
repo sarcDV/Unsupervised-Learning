fcf14b8aaee7b6a5f1732cc51b7d9588e4325d3b
sarcDV <alex.sciarra@gmail.com>
2023-03-17 08:06:53 +0100

    Create threeD-convolutional-VAE.py

---
+++ NetworkModels/threeD-convolutional-VAE.py
@@ -0,0 +1,70 @@
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+latent_dim = 2
+
+encoder_inputs = keras.Input(shape=(32, 32, 32, 1))
+x = layers.Conv3D(32, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
+x = layers.Conv3D(64, 3, activation="relu", strides=2, padding="same")(x)
+x = layers.Conv3D(128, 3, activation="relu", strides=2, padding="same")(x)
+x = layers.Flatten()(x)
+x = layers.Dense(16, activation="relu")(x)
+z_mean = layers.Dense(latent_dim, name="z_mean")(x)
+z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
+z = layers.Lambda(sampling)([z_mean, z_log_var])
+encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+encoder.summary()
+
+latent_inputs = keras.Input(shape=(latent_dim,))
+x = layers.Dense(4 * 4 * 4 * 128, activation="relu")(latent_inputs)
+x = layers.Reshape((4, 4, 4, 128))(x)
+x = layers.Conv3DTranspose(128, 3, activation="relu", strides=2, padding="same")(x)
+x = layers.Conv3DTranspose(64, 3, activation="relu", strides=2, padding="same")(x)
+decoder_outputs = layers.Conv3DTranspose(1, 3, activation="sigmoid", padding="same")(x)
+decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+decoder.summary()
+
+class VAE(keras.Model):
+    def __init__(self, encoder, decoder, **kwargs):
+        super(VAE, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+        self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
+        self.reconstruction_loss_tracker = keras.metrics.Mean(
+            name="reconstruction_loss"
+        )
+        self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
+
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.kl_loss_tracker,
+        ]
+
+    def train_step(self, data):
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var, z = self.encoder(data)
+            reconstruction = self.decoder(z)
+            reconstruction_loss = tf.reduce_mean(
+                keras.losses.binary_crossentropy(data, reconstruction)
+            )
+            kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
+            total_loss = reconstruction_loss + kl_loss
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+        }
+
+vae = VAE(encoder, decoder)
+vae.compile(optimizer=keras.optimizers.Adam())
